c9ab9a7ed7580fedb4539c6d5d157998ec36865e
rakesh <rakeshcpr011@gmail.com>
2026-05-15 09:51:34 +0530

    tests

---
+++ test/test_operation.py
@@ -0,0 +1,13 @@
+from src.math_operations import add, multiply
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 5) == 0
+        
